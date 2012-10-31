275a2c5aa259edb0519c638b21c6e00d1126d15f
titopez <roberto.rpr@gmail.com>
2012-10-31 02:12:55 -0400

    creado el modelo de datos y creada la BD

---
+++ aplicacion/Agenda1/CADAgenda1/ScrumBDModel.edmx.sql
@@ -0,0 +1,117 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, and Azure
+-- --------------------------------------------------
+-- Date Created: 10/31/2012 02:05:49
+-- Generated from EDMX file: D:\repositoriosGit\sis325\aplicacion\Agenda1\CADAgenda1\ScrumBDModel.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [ScrumBD];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Proyectos'
+CREATE TABLE [dbo].[Proyectos] (
+    [id] int IDENTITY(1,1) NOT NULL,
+    [Nombre] nvarchar(60)  NOT NULL,
+    [FechaInicio] datetime  NOT NULL,
+    [FechaFinalizacion] datetime  NOT NULL,
+    [Objetivo] nvarchar(max)  NOT NULL,
+    [CajaTiempo] int  NOT NULL
+);
+GO
+
+-- Creating table 'Historias'
+CREATE TABLE [dbo].[Historias] (
+    [id] int IDENTITY(1,1) NOT NULL,
+    [Descripcion] nvarchar(max)  NOT NULL,
+    [Prioridad] int  NOT NULL,
+    [Habilitado] bit  NOT NULL,
+    [Proyecto_id] int  NOT NULL
+);
+GO
+
+-- Creating table 'Roles'
+CREATE TABLE [dbo].[Roles] (
+    [id] int IDENTITY(1,1) NOT NULL,
+    [NombreCompleto] nvarchar(max)  NOT NULL,
+    [Responsabilidad] nvarchar(max)  NOT NULL,
+    [ResponsabilidadSecundaria] nvarchar(max)  NOT NULL,
+    [Proyecto_id] int  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [id] in table 'Proyectos'
+ALTER TABLE [dbo].[Proyectos]
+ADD CONSTRAINT [PK_Proyectos]
+    PRIMARY KEY CLUSTERED ([id] ASC);
+GO
+
+-- Creating primary key on [id] in table 'Historias'
+ALTER TABLE [dbo].[Historias]
+ADD CONSTRAINT [PK_Historias]
+    PRIMARY KEY CLUSTERED ([id] ASC);
+GO
+
+-- Creating primary key on [id] in table 'Roles'
+ALTER TABLE [dbo].[Roles]
+ADD CONSTRAINT [PK_Roles]
+    PRIMARY KEY CLUSTERED ([id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [Proyecto_id] in table 'Roles'
+ALTER TABLE [dbo].[Roles]
+ADD CONSTRAINT [FK_RolesDeUnProyecto]
+    FOREIGN KEY ([Proyecto_id])
+    REFERENCES [dbo].[Proyectos]
+        ([id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_RolesDeUnProyecto'
+CREATE INDEX [IX_FK_RolesDeUnProyecto]
+ON [dbo].[Roles]
+    ([Proyecto_id]);
+GO
+
+-- Creating foreign key on [Proyecto_id] in table 'Historias'
+ALTER TABLE [dbo].[Historias]
+ADD CONSTRAINT [FK_HistoriasDeUnProyecto]
+    FOREIGN KEY ([Proyecto_id])
+    REFERENCES [dbo].[Proyectos]
+        ([id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_HistoriasDeUnProyecto'
+CREATE INDEX [IX_FK_HistoriasDeUnProyecto]
+ON [dbo].[Historias]
+    ([Proyecto_id]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
